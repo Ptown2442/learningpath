878cd781c0ce4f547188f533221ed1c08380c4a9
Ptown2442 <portervaughn93@gmail.com>
2023-08-24 19:53:25 -0700

    first git

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let _ = require('underscore');
 
-let result = _.contains([1, 2, 3], 2);
+const Joi = require('joi');
+const home = require('./routes/home');
+const courses = require('./routes/courses');
+const dbDebugger = require('debug')('app:db');
+const startupDebugger = require('debug')('app:startup');
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
 
-console.log(result);
+app.use(express.json());
+app.use('/api/courses', courses);
+app.use('/', home);
+
+app.use(function(req, res, next) {
+    console.log('Logging...');
+    next();
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
